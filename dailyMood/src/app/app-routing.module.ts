dc8b47bed000c2dacfae88a7a9599f73d4c3824b
mysticBel <dnxbel@gmail.com>
2021-10-19 09:46:21 -0500

    adding routing

--- dailyMood/src/app/app-routing.module.ts
+++ dailyMood/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateRecordComponent } from './components/create-record/create-record.component';
+import { ListDaysComponent } from './components/list-days/list-days.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'overview', component:ListDaysComponent },
+  { path: 'add-record', component:CreateRecordComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
